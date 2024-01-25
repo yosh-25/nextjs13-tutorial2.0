6d4bb6060d68b1b764d8d4ff60203be8e8e164e3
yosh-25 <yoshi.okada25@gmail.com>
2024-01-25 10:40:01 +0900

    aaa

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,24 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 
 type Todo = {
-    title: string;
+  title: string;
 };
 
 async function getData() {
-    const res = await fetch("http://localhost:3000/api/todos");
-    return res.json();
+  const res = await fetch("http://localhost:3000/api/todos", {
+    next: {revalidate: 10,
+}});
+  return res.json();
 }
 
 export default async function Page() {
-    const todos: Todo[] = await getData();
+  const todos: Todo[] = await getData();
 
-    return (
+  return (
     <>
-    <h1>Todos</h1>
-    {todos.map((todo) => (
+      <h1>Todos</h1>
+      {todos.map((todo) => (
         <div>{todo.title}</div>
-        ))}
-    <Link href="/">Home</Link>
+      ))}
+      <Link href="/">Home</Link>
     </>
-    )
-  }
+  );
+}
--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -11,7 +11,7 @@ const todos: Todo[] = [
 ];
 
 export default async (req: NextApiRequest, res: NextApiResponse)  => {
-    await new Promise((resolve)=> setTimeout((resolve),4000));
+    await new Promise((resolve)=> setTimeout((resolve), 4000));
 
     res.status(200).json(todos);
 };
